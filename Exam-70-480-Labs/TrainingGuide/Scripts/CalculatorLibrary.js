6497e14445034dd763f7de4438332307fed925e6
Samy_Hammami <samy.Hammami@hotmail.com>
2014-11-17 16:04:24 +0000

    Chapter 7 Forms and Forms Validations

--- Exam-70-480-Labs/TrainingGuide/Scripts/CalculatorLibrary.js
+++ Exam-70-480-Labs/TrainingGuide/Scripts/CalculatorLibrary.js
@@ -4,148 +4,71 @@
     this.calculatorNamespace = this.calculatorNamespace || {};
     var ns = this.calculatorNamespace;
 
-    var operator;
-    ns.initialize = function() {
-
+    ns.initialize = function () {
         var calculator = new ns.Calculator();
-
-
-
         $('button[id^="btnNumber"]').on('click', calculator.numberClick);
-
-        /*
-        for (var i = 0; i < 10; i++) {
-            document.getElementById('btn' + i).addEventListener('click',
-            numberClick, false);
-        }
-        */
-
-
-        // document.getElementById('btnPlus').addEventListener('click', plusClick, false);
         $('#btnPlus').on('click', calculator.plusClick);
-
-        //document.getElementById('btnMinus').addEventListener('click', minusClick, false);
         $('#btnMinus').on('click', calculator.minusClick);
-
-        //document.getElementById('btnMultiply').addEventListener('click', multiplyClick, false);
-        $('#btnMultiply').on('click', calculator.multiplyClick);
-
-        //document.getElementById('btnDivide').addEventListener('click', divideClick, false);
-        $('#btnDivide').on('click', calculator.divideClick);
-
-        // document.getElementById('btnCalculate').addEventListener('click', calculateClick, false);
-        $('#btnCalculate').on('click', calculator.calculateClick);
-
-        // document.getElementById('btnClearEntry').addEventListener('click', clearEntry, false);
         $('#btnClearEntry').on('click', calculator.clearEntry);
-
-
-        //document.getElementById('btnClear').addEventListener('click', clear, false);
         $('#btnClear').on('click', calculator.clear);
         calculator.clear();
     }
 
+    ns.ContactUsInitialize = function () {
+        //$('#filter').blur(function () {
+        //    $('#options').hide();
+        //});
+        $('#submit').hide();
+        //$('#ContactForm').validate();
+
+
+        $('form input').blur(function () {
+            if ($('#ContactForm').valid()) {
+                $('#submit').show();
+            }
+            else {
+                $('#submit').visible=false;
+            }
+        });
+      
+    }
+
+
     ns.Calculator = (function () {
 
-
         function Calculator() {
-
         }
 
-
         Calculator.prototype.numberClick = function () {
-
-            /*txtInput.value = txtInput.value == '0' ?
-            this.innerText : txtInput.value + this.innerText;*/
-
-           /* $('#txtInput').val($('#txtInput').val() == '0'?
-                $(this).text()  : $('#txtInput').val + $(this).text()
-                );*/
-
             $('#txtInput').val($('#txtInput').val() == '0' ?
-                            $(this).text() : $('#txtInput').val() + $(this).text());
+                $(this).text() : $('#txtInput').val() + $(this).text());
         };
 
         Calculator.prototype.plusClick = function () {
-
-            Calculator.prototype.calculateClick();
-            operator = '+';
+            $('#txtResult').val(Number($('#txtResult').val()) +
+                Number($('#txtInput').val()));
             Calculator.prototype.clearEntry();
         };
 
-        Calculator.prototype.minusClick = function () {
-            Calculator.prototype.calculateClick();
-            operator = '-';
-            Calculator.prototype.clearEntry();
-        };
-
-        Calculator.prototype.multiplyClick = function () {
-            /*
-            if (txtInput.value != '0')
-                txtResult.value = txtInput.value;*/
-
-            if ($('#txtInput').val() != '0')
-                $('#txtResult').val($('#txtInput').val());
-
-
-            Calculator.prototype.calculateClick();
-            operator = '*';
-            Calculator.prototype.clearEntry();
-        };
-
-        Calculator.prototype.divideClick = function () {
-
-
-            /*
-            if (txtInput.value != '0')
-                txtResult.value = txtInput.value;
-            operator = '/';
-            clearEntry();*/
-
-            if ($('#txtInput').val() != '0')
-                $('#txtResult').val($('#txtInput').val());
-            operator = '/';
-            Calculator.prototype.clearEntry();
-           
-        };
-
-        Calculator.prototype.calculateClick = function () {
-            switch (operator) {
-                case '*':
-                    txtResult.value = Number(txtResult.value) * Number(txtInput.value);
-                    break;
-                case '/':
-                    txtResult.value = Number(txtResult.value) / Number(txtInput.value);
-                    break;
-                case '+':
-                    txtResult.value = Number(txtResult.value) + Number(txtInput.value);
-                    break;
-                case '-':
-                    txtResult.value = Number(txtResult.value) - Number(txtInput.value);
-                    break;
-                default:
-                    break;
-            }
-            Calculator.prototype.clearEntry();
-        };
+    Calculator.prototype.minusClick = function () {
+    $('#txtResult').val(Number($('#txtResult').val()) -
+        Number($('#txtInput').val()));
+    Calculator.prototype.clearEntry();
+};
 
         Calculator.prototype.clearEntry = function () {
             $('#txtInput').val('0');
-
-           // txtInput.value = '0';
         };
 
-
         Calculator.prototype.clear = function () {
-/*
-            txtInput.value = '0';
-            txtResult.value = '0';*/
             $('#txtInput').val('0');
             $('#txtResult').val('0');
-            
         };
 
         return Calculator;
     }());
 
 })();
+
+
+
--- Exam-70-480-Labs/TrainingGuide/Scripts/_references.js
+++ Exam-70-480-Labs/TrainingGuide/Scripts/_references.js
@@ -1,3 +1,4 @@
 ﻿/// <reference path="jquery-2.1.1.js" />
+/// <reference path="jquery.validate.js" />
 /// <reference path="qunit.js" />
 /// <reference path="CalculatorLibrary.js" />
